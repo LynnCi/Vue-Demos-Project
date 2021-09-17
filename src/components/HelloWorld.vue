2ecb3ad4c073b170cd75d3af045d89c0d7eca537
Celine.Cui <linn21@163.com>
2021-09-17 23:30:05 +0800

    feat:add es6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,11 @@
   <div class="hello">
     <h1>vue demos</h1>
     <ol>
-      <li><promise-demo></promise-demo></li>
+      <!-- <li><promise-demo></promise-demo></li> -->
       <!-- <li><test-promise></test-promise> </li> -->
       <!-- <li> <async-await></async-await> </li> -->
       <!-- <li> <img-promise></img-promise> </li> -->
+      <li><es6demo></es6demo></li>
     </ol>
   </div>
 </template>
@@ -15,13 +16,15 @@ import PromiseDemo from './demoPromise'
 import TestPromise from './demoPromise/testPromise.vue'
 import AsyncAwait from './demoPromise/asyncAwait.vue'
 import ImgPromise from './demoPromise/imgPromise.vue'
+import es6demo from './demoES6'
 export default {
   name: 'HelloWorld',
   components:{
     PromiseDemo,
     TestPromise,
     AsyncAwait,
-    ImgPromise
+    ImgPromise,
+    es6demo
   },
   data () {
     return {
---
+++ src/components/demoES6/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>es6</div>
+</template>
+
+<script>
+// import { jiegou } from './index'
+export default{
+    name:'index',
+    created(){
+        this.set()
+    },
+    methods:{
+        //对象和数组解构
+        demo1(){
+            const object = {
+                name:'Joe',
+                age:9,
+                sex:'male'
+            }
+            let { name,age,sex } = object
+            console.log('age is' ,age)
+
+            let [a,,c] = [1,2,3]
+            console.log(`a is ${a}`)
+        },
+        //for of 遍历数组 & for in遍历对象中的属性
+        demo2(){
+            let array = ['aaa','bbb','cc']
+            for(let i of array){
+                console.log('i',i)
+            }
+            for(let k in array){
+                console.log('k',k,'item',array[k])
+            }
+        },
+        //...扩展运算符 + 默认参数 
+        demo3(){
+            let arr = [10,11,12]
+            let newArr = [30,...arr,20]
+            console.log('newArr',newArr)
+
+            let object = {
+                sex:'female',
+                age:'9'
+            }
+            let newObject = {...object,name:'susie'}
+            console.log('newObject',newObject)
+        },
+        //模版字符串
+        demo4(){
+            let name = 'olivia',age = 8
+            console.log(`my name is ${name}, and i'm ${age} years old`)
+        },
+        //set不会添加重复的值
+        set(){
+            const s = new Set();
+            [3,1,2,1,3,4,2,3].forEach(x => s.add(x))
+            for(let i of s){
+                console.log('i',i)
+            }
+            //数组祛重[...new Set(array)]
+            const set1 = new Set([1,2,3,3])
+            let newSet = [...set1]
+            console.log('newSet',newSet) //1,2,3
+
+            const set2 = new Set([1,2,3,3,4,5,5,5])
+            console.log('set2',set2.size) //5
+
+        },
+    }
+}
+</script>
